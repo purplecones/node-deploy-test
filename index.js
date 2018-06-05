9a035b917e14eeacd9bb2af73ede9d41e486da82
Mirza Joldic <mirza.joldic@gmail.com>
2018-06-04 21:40:38 -0400

    poc

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = Hapi.server({
+  port: 8000,
+});
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, h) {
+    return `Process on the server: ${process.pid}`;
+  },
+});
+
+// Start the server
+async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log('Server running at:', server.info.uri);
+}
+
+start();
